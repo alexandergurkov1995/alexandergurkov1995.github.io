a6f6367d28432b9f026a1a747e39cfa34edef514
alexandergurkov1995 <34387184+alexandergurkov1995@users.noreply.github.com>
2018-01-28 02:08:09 +0300

    mosaquastore-ru

---
+++ mosaquastore-ru/js/ratio-activate.js
@@ -0,0 +1,63 @@
+$(function() {
+
+            $("#rateYo").rateYo({
+                rating: 3.6
+            });
+
+        });
+        
+        $(function() {
+
+            $("#rateYo1").rateYo({
+                rating: 3.6
+            });
+
+        });
+        
+        $(function() {
+
+            $("#rateYo2").rateYo({
+                rating: 3.6
+            });
+
+        });
+        
+        $(function() {
+
+            $("#rateYo3").rateYo({
+                rating: 3.6
+            });
+
+        });
+        
+        $(function() {
+
+            $("#rateYo4").rateYo({
+                rating: 3.6
+            });
+
+        });
+        
+        $(function() {
+
+            $("#rateYo5").rateYo({
+                rating: 3.6
+            });
+
+        });
+        
+        $(function() {
+
+            $("#rateYo6").rateYo({
+                rating: 3.6
+            });
+
+        });
+        
+        $(function() {
+
+            $("#rateYo7").rateYo({
+                rating: 3.6
+            });
+
+        });
